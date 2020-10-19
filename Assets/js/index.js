e4899ff3ee900e73c8f82b48b6d74dbc4b0c5044
Ben Galloway <bdgalloway85@gmail.com>
2020-10-18 20:06:10 -0500

    create submit button listener

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,4 +1,65 @@
 $(document).ready(function () {
+    //store city name
+    var recentCityNames = [];
 
-    
-}
+    $('#submit').on('click', function (event) {
+        console.log(event);
+        event.preventDefault();
+
+        //take imput city
+        var input = $('input').val();
+        console.log(input);
+
+    });
+
+
+    //make ajax call
+
+    function getWeatherData() { }
+
+
+
+
+
+
+
+
+
+
+    //return current weather
+    function renderCurrentWeather() {
+
+    };
+
+
+    //return 5-day forcast
+    function renderForcast() {
+
+    }
+
+    //save city as a button
+
+    function createCityButton() {
+
+    }
+    function appendCityButton() {
+
+    }
+
+    //add to local storage, recently search cities list
+
+    function saveRecentSearchToList() {
+
+    }
+
+    function hydrateDataFromLocalstorage() {
+
+    }
+
+    function renderButtons() {
+
+    }
+
+
+
+});
