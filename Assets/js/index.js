62342ff01e5c0c57f1a569a4036b290b0ad130d7
Ben Galloway <bdgalloway85@gmail.com>
2020-10-18 22:03:32 -0500

    render UV data

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -11,47 +11,43 @@ $(document).ready(function () {
         getCurrentWeatherData(inputCity);
     });
 
-
     //make ajax call
-
     function getCurrentWeatherData(city) {
-
-
         var queryURL = "http://api.openweathermap.org/data/2.5/weather?appid=16e2a29d08bf4766fcdb6563c3920b3d&units=imperial&q=" + city;
-
         $.ajax({
             url: queryURL,
             method: "GET"
         })
-        .then(function(res) {
-            renderCurrentWeather(res);
-        })
-
+            .then(function (res) {
+                renderCurrentWeather(res);
+            })
     }
 
     //return current weather
     function renderCurrentWeather(info) {
-        console.log(info);
-        
         var cityName = info.name;
         var dateTime = info.dt;
-
         var currentDate = dayjs.unix(dateTime).format('MM/DD/YYYY');
-        
-
-
-
+        var lat = info.coord.lat;
+        var lon = info.coord.lon;
 
         //render current weather data
+        renderUV(lat, lon);
         $('#city-n-date').text(cityName + " (" + currentDate + ")");
         $('#city-temp').html("Temprature: " + info.main.temp + "&#8457");
         $('#city-humi').html("Humidity: " + info.main.humidity + "%");
         $('#city-wind').html("Wind Speed: " + info.wind.speed + " MPH");
-        $('#city-uv').html("UV Index: ");
-
+        function renderUV(latitude, longitude) {
+            var queryURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=16e2a29d08bf4766fcdb6563c3920b3d";
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+                .then(function (res) {
+                    $('#city-uv').text("UV Index: " + res.value);
+                });
+        }
     };
-
-
     //return 5-day forcast
     function renderForcast() {
 
