a26cc576874772097fd58ccb8c4e3afb170e0734
Ben Galloway <bdgalloway85@gmail.com>
2020-10-19 12:10:02 -0500

    hydrate page data from storage

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,16 +1,36 @@
 $(document).ready(function () {
     //store city name
     var recentCityNames = [];
+    hydrateDataFromLocalstorage();
+    //console.log(recentCityNames);
+
 
     $('#submit').on('click', function (event) {
         //console.log(event);
         event.preventDefault();
 
+
         //take imput city
         var inputCity = $('input').val();
         getCurrentWeatherData(inputCity);
+        buttonFactory(inputCity);
+        saveRecentSearchToList(inputCity);
+        console.log(inputCity);
+        console.log(recentCityNames);
     });
 
+
+    addButtonListener();
+    function addButtonListener() {
+        var $button = $('#button-list').children();
+        $.each($button, function () {
+            $button.on('click', function (evt) {
+                evt.preventDefault();
+                //console.log(evt.target.textContent);
+                getCurrentWeatherData(evt.target.textContent);
+            });
+        });
+    }
     //make ajax call
     function getCurrentWeatherData(city) {
         var queryURL = "http://api.openweathermap.org/data/2.5/weather?appid=16e2a29d08bf4766fcdb6563c3920b3d&units=imperial&q=" + city;
@@ -55,27 +75,34 @@ $(document).ready(function () {
 
     //save city as a button
 
-    function createCityButton() {
+    function buttonFactory(cityName) {
+        var $button = $('<button>')
+        $button.text(cityName);
+        $button.attr('class', 'button is-block m-1');
+        $('#button-list').prepend($button);
+        addButtonListener();
 
     }
-    function appendCityButton() {
-
-    }
-
     //add to local storage, recently search cities list
+    function saveRecentSearchToList(city) {
+        console.log(recentCityNames);
+        recentCityNames.push(city);
+        localStorage.setItem('recentCityStorage', JSON.stringify(recentCityNames));
 
-    function saveRecentSearchToList() {
+
+        //console.log(recentCityNames);
 
     }
 
     function hydrateDataFromLocalstorage() {
-
+        console.dir(recentCityNames);
+        var recentCityStorage = JSON.parse(localStorage.getItem('recentCityStorage'));
+        if (recentCityStorage !== null) {
+            recentCityNames = recentCityStorage;
+            for (let i = 0; i < recentCityNames.length; i++) {
+                buttonFactory(recentCityNames[i]);
+            }
+        }
     }
-
-    function renderButtons() {
-
-    }
-
-
 
 });
