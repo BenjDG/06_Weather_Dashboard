8f98b4be5a441df1eb61dae4ff807d889e73838b
Ben Galloway <bdgalloway85@gmail.com>
2020-10-19 22:46:52 -0500

    clean up whitespaces

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -2,35 +2,16 @@ $(document).ready(function () {
     //store city name
     var recentCityNames = [];
     hydrateDataFromLocalstorage();
-    //console.log(recentCityNames);
-
-
     $('#submit').on('click', function (event) {
-        //console.log(event);
         event.preventDefault();
-
-
+        $('#main-page').removeClass('is-hidden');
         //take imput city
         var inputCity = $('input').val();
+        $('input').val("");
         getCurrentWeatherData(inputCity);
         buttonFactory(inputCity);
         saveRecentSearchToList(inputCity);
-        //console.log(inputCity);
-        //console.log(recentCityNames);
     });
-
-
-    addButtonListener();
-    function addButtonListener() {
-        var $button = $('#button-list').children();
-        $.each($button, function () {
-            $button.on('click', function (evt) {
-                evt.preventDefault();
-                //console.log(evt.target.textContent);
-                getCurrentWeatherData(evt.target.textContent);
-            });
-        });
-    }
     //make ajax call
     function getCurrentWeatherData(city) {
         var queryURL = "http://api.openweathermap.org/data/2.5/weather?appid=16e2a29d08bf4766fcdb6563c3920b3d&units=imperial&q=" + city;
@@ -42,7 +23,6 @@ $(document).ready(function () {
                 renderCurrentWeather(res);
             })
     }
-
     //return current weather
     function renderCurrentWeather(info) {
         var cityName = info.name;
@@ -50,11 +30,7 @@ $(document).ready(function () {
         var currentDate = dayjs.unix(dateTime).format('MM/DD/YYYY');
         var lat = info.coord.lat;
         var lon = info.coord.lon;
-
-        //call forecast getData
         getForcast(lat, lon);
-
-        //render current weather data
         renderUV(lat, lon);
         $('#city-n-date').text(cityName + " (" + currentDate + ")");
         $('#city-temp').html("Temprature: " + info.main.temp + "&#8457");
@@ -67,12 +43,16 @@ $(document).ready(function () {
                 method: "GET"
             })
                 .then(function (res) {
-                    $('#city-uv').text("UV Index: " + res.value);
+                    if (res.value < 3) {
+                        $('#city-uv').html(" " + res.value).attr('class', 'has-background-success');
+                    } else if (res.value >= 3 && res.value < 8) {
+                        $('#city-uv').html(" " + res.value).attr('class', 'has-background-warning');
+                    } else {
+                        $('#city-uv').html(" " + res.value).attr('class', 'has-background-danger');
+                    }
                 });
         }
     };
-
-
     //return 5-day forcast
     function getForcast(lat, lon) {
         var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=current,minutely,hourly,alerts&appid=16e2a29d08bf4766fcdb6563c3920b3d";
@@ -81,56 +61,43 @@ $(document).ready(function () {
             method: "GET"
         })
             .then(function (res) {
-                console.log(res.daily[0].weather[0].description);
-                // console.log(res.daily[0].humidity);
-                // console.log(res.daily[0].dt);
-                // console.log(dayjs.unix(res.daily[0].dt).format('MM/DD/YYYY'));
-                // console.log(res.daily[0].feels_like.day);
-                // console.log(toF(res.daily[0].feels_like.day));
-                //var $forecastCard = $('#forecast-card > div > p >');
-                //var $fdate = $forecastCard.find('fdate');
-                //console.dir($forecastCard);
-
                 $('#forecast-card').empty();
                 for (var i = 0; i < 5; i++) {
                     $div = $('<div>').attr('class', 'tile is-block');
                     $date = $('<p>').text(dayjs.unix(res.daily[i].dt).format('MM/DD/YYYY'));
-                    $image = $('<img>').attr('src','http://openweathermap.org/img/wn/' + res.daily[i].weather[0].icon + '@2x.png').attr('alt', res.daily[i].weather[0].description);
+                    $image = $('<img>').attr('src', 'http://openweathermap.org/img/wn/' + res.daily[i].weather[0].icon + '@2x.png').attr('alt', res.daily[i].weather[0].description);
                     $temp = $('<p>').html("Temp: " + toF(res.daily[i].feels_like.day) + "&#8457");
                     $humi = $('<p>').text("Humidity: " + res.daily[i].humidity + "%");
                     $div.append($date, $image, $temp, $humi);
                     $('#forecast-card').append($div);
                 }
-
-                // //res.list[0].dt
-                // var ftime = dayjs.unix(res.list[3].dt).format('MM/DD/YYYY');
-                // console.log(ftime);
             })
     }
-
     //save city as a button
-
     function buttonFactory(cityName) {
         var $button = $('<button>')
         $button.text(cityName);
         $button.attr('class', 'button is-block m-1');
         $('#button-list').prepend($button);
         addButtonListener();
-
+    }
+    //addButtonListener();
+    function addButtonListener() {
+        var $button = $('#button-list').children();
+        $button.each(function () {
+            $button.on('click', function (evt) {
+                //evt.preventDefault();
+                $('#main-page').removeClass('is-hidden');
+                getCurrentWeatherData(evt.target.textContent);
+            });
+        });
     }
     //add to local storage, recently search cities list
     function saveRecentSearchToList(city) {
-        //console.log(recentCityNames);
         recentCityNames.push(city);
         localStorage.setItem('recentCityStorage', JSON.stringify(recentCityNames));
-
-
-        //console.log(recentCityNames);
-
     }
-
     function hydrateDataFromLocalstorage() {
-        //console.dir(recentCityNames);
         var recentCityStorage = JSON.parse(localStorage.getItem('recentCityStorage'));
         if (recentCityStorage !== null) {
             recentCityNames = recentCityStorage;
@@ -139,10 +106,8 @@ $(document).ready(function () {
             }
         }
     }
-
     function toF(k) {
         var f = (k - 273.15) * 9 / 5 + 32;
         return f.toFixed();
     }
-    //console.log(toF(300));
 });
